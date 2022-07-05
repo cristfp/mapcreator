65cbc8979e4075a77e95d6857971c20b6538063f
cristfp <cristfp@hotmail.com>
2022-07-05 03:12:45 -0300

    second commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Core\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('store', [StoreController::class, 'index']);
+Route::post('store', [StoreController::class, 'store']);
+Route::put('store/{id}', [StoreController::class, 'update']);
+Route::delete('store/{id}', [StoreController::class, 'destroy']);
